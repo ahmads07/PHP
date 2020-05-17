25fbebd2005fdd6971a28d942e85f0ab5ac84b10
ahmads07 <64453165+ahmads07@users.noreply.github.com>
2020-05-18 10:38:50 +1200

    class1 for echo elements

---
+++ week5/class1html.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create PHP in HTML</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php
+$i = 1;
+while ($i < 100){ //set condition
+    echo "<p>Class is over, come back tomorrow</p>"; //do something if condition is true
+    $i = $i + 1; //update counter
+?>
+</body>
+</html>
