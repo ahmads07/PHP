6d05c532f4082f1b9113ffdee5098f7f79b3432b
ahmads07 <64453165+ahmads07@users.noreply.github.com>
2020-05-18 10:29:09 +1200

    class1 for echo elements

--- week5/class1.php
+++ week5/class1.php
@@ -1,3 +1,4 @@
+
 <?php
 
 echo "hello world"; //echo means print
@@ -7,3 +8,9 @@ echo "<h1>Hello World</h1>";
 echo "<p>Hello World</p>";
 
 echo "<p style='color: red'>Hello World</p>";
+
+$i = 0; //set counter
+while ($i < 100){ //set condition
+    echo "<p>Hello</p>"; //do something if condition is true
+    $i = $i + 1; //update counter
+}
