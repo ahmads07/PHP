6c1910b690651e922c7fa0b12c16f1bcd5cde567
Salman Ahmad (ISG) <salman.ahmad@fbu.com>
2020-05-19 09:08:15 +1200

    update

---
+++ class2.php
@@ -0,0 +1,15 @@
+<?php
+
+$grating = "hello"; // $ is the syntax for variable in php
+$who = "Lei";
+
+echo $grating;
+echo $who
+
+echo "<h1>" . $grating . " " . $who . "</h1>"; //dot means merge string in php
+
+$x = 4;
+$y = 5;
+echo "x = ".$x;
+echo "y = ".$y;
+echo "z = x + y = ".($x + $y);
