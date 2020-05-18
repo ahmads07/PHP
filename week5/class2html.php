0e269ee711ab88ac123a60c063f95832e17afc6c
Salman Ahmad (ISG) <salman.ahmad@fbu.com>
2020-05-19 10:01:14 +1200

    update

---
+++ week5/class2html.php
@@ -0,0 +1,9 @@
+<?php
+//set default timezone
+date_default_timezone_set('Pacific/Auckland');
+//take the time from system or online
+//data function to take the time out from system or online
+$t = date("Y-m-d"); //https://www.php.net/manual/en/function.date.php
+$t1 = date("H:i:s");
+echo $t." ".$t1;
+
