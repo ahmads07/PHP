23882869e869bbecf0a7cda6a9d112894992e6c5
Salman Ahmad (ISG) <salman.ahmad@fbu.com>
2020-05-19 09:30:13 +1200

    update

--- week5/class2.php
+++ week5/class2.php
@@ -13,3 +13,14 @@ $y = 5;
 echo "x = ".$x;
 echo "y = ".$y;
 echo "z = x + y = ".($x + $y);
+
+//length of string (strlen)
+echo "<br>".strlen($grating)."string length<br>";
+//word count -- count number of words in that string
+echo str_word_count($grating)."string word count<br>";
+//string reverse
+echo strrev($grating)."string reverse<br>";
+//string position
+echo strpos($grating, "hello")."word position<br>";
+//string replace
+echo str_replace($grating, "hello", "byebye")."word replace<br>";
