ed1cd684a23808e294ada66454e4bc1722b0f01c
Salman Ahmad (ISG) <salman.ahmad@fbu.com>
2020-05-20 09:14:57 +1200

    update

--- week5/form.php
+++ week5/form.php
@@ -1,11 +1,16 @@
 <?php
 ?>
+<p>action is the url you send your request</p>
+<p>method has get and post. get is you request a data from server, post is server ask the send data to it and process for you</p>
+<p>method in form, is your request method</p>
+<p>name inside input box is your request data name</p>
 
 <form action="result.php" method="post">
     <p>
-        Mark: <input type="=number" name="mark">
+        mark: <input type="number" name="mark">
     </p>
     <p>
         <input type="submit" value="show">
     </p>
 </form>
+
