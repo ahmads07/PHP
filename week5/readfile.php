168900e27ad16015971e8dea1d8393834fbbbde0
Salman Ahmad (ISG) <salman.ahmad@fbu.com>
2020-05-20 10:18:55 +1200

    read file

--- week5/readfile.php
+++ week5/readfile.php
@@ -3,6 +3,6 @@
 $file = fopen("helloworld.txt", 'r'); //fopen all programming language for file open, r means read only,
                                                         //w is write, a is append
 while (!feof($file)){
-    echo fgets($file)."<br>";
+    echo fgets($file).",";
 }
 fclose($file);
